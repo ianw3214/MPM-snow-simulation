24b1e66a678025e717d1f4efcfcf036d80d0cf24
Ian <ianw3214@gmail.com>
2020-12-20 15:38:05 -0500

    Profiling update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <ground.hpp>
 #include <iostream>
 #include <memory>
+#include <chrono>
 
 int main(int argc, char *argv[]) {
   Constants constants;
@@ -11,7 +12,10 @@ int main(int argc, char *argv[]) {
   simulation.Init();
   simulation.add_collision_object(std::make_unique<Ground>(0));
 
+  auto updateStart = std::chrono::system_clock::now();
   simulation.Update(0.016);
+  double time_elapsed = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - updateStart).count());
+  std::cout << "Time elapsed: " << time_elapsed << "us (" << time_elapsed / 1000000.0 << " s)" << std::endl;
 
   std::cout << "Simulation finished." << std::endl;
 
